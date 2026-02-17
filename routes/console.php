3df0351d1fca774ccd9ebeeff08defa56e3723ac
cwalihyder <cw.alihyder@gmail.com>
2026-02-17 18:39:14 +0500

    refactor: scheudle

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+for ($i = 0; $i < 5; $i++) {
+    Schedule::command('blog:generate')->dailyAt('15:00');
+}
